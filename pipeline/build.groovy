10e4100e0bad7e864b0e1c86e7a32aa350dad520
hardikhard28 <63140787+hardikhard28@users.noreply.github.com>
2023-06-04 17:11:57 +0530

    adding groovy script

---
+++ pipeline/build.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    // Use the checkout step to clone the Git repository
+                    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/master']], // Specify the branch you want to checkout
+                        userRemoteConfigs: [[url: 'https://github.com/hardikhard28/springbootapp.git']] // Specify your Git repository URL
+                    ])
+                }
+            }
+        }
+
+        
+    }
+}
